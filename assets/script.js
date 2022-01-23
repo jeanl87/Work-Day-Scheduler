a71b564a769b160166c685b0116db6be8f9bd0fb
Lisa Jean <MsJean@Lisas-Air.fios-router.home>
2022-01-23 18:05:44 -0500

    doing local storage

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
-let currentDate = moment().format("MMMM Do YYYY");
+let currentDate = moment().format("MMMM DD YYYY");
 let displayDate = document.getElementById("currentDay");
+displayDate.textContent = currentDate
 
 
 let taskInput =(".row textarea[type=text]");
 
 $(".time-div"). each(function () {
-    var timeDiv = $(this).attr("id")
-    if(currentHour > timeDiv){
-        $(this).removeClass("future");
-        $(this).addClass("past");
-    }
-    else if(currentHour == timeDiv){
+    let timeDiv = currentHour;
+    var timeDiv = $(this).attr("id");
+
+    if(currentHour == timeDiv){
         $(this).addClass("present");
     }
     else if(currentHour < timeDiv){
         $(this).addRemove("present");
-        $(this).addClass("future"); 
+        $(this).addClass("future");
+    }
+    else if(currentHour > timeDiv){
+        $(this).addRemove("future");
+        $(this).addClass("past"); 
     }
 }
 );
 
-    $(".saveBtn").on("click", function(){
-        var time = $(this).parent().attr('id')
-        var value = $(this).siblings(".time-block").val()
+   $(".saveBtn").on("click", function(){
+        var time = $(this).parent().attr('id').split("-")[1]
+        var value = $(this).siblings(".description").val()
         localStorage.setItem(time,value);
     });
-$("#9").val(localStorage.getItem("9")); 
+$("#.descripton").val(localStorage.getItem("9")); 
 
 
 
